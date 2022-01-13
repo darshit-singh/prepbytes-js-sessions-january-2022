5e50a0b60d4bf891e6ca4ee2d3d55caed7c903e3
Darshit Sarna <op.dare7@gmail.com>
2022-01-13 23:46:26 +0530

    session 1

--- Session1/script.js
+++ Session1/script.js
@@ -1,1 +1,19 @@
-console.log('hi')
+//GLOBAL execution context.
+var a = 4;
+// debugger; // allows me to stop execution at this line.
+
+console.log(a * 3);
+
+function multiplyBy2(num) { //parameter
+    // return num * 2;
+    console.log(num * 2); //printing twice the value of num.
+}
+
+//function invocation - calling the function.
+// calling the function in order to execute.
+
+multiplyBy2(a);
+
+//keywords - var, function, for, while
+
+//predefined function names built in browser - innerWidth, setTimeout
