f2b9ecd5656dbc7c503ee7b578f8645a4636a13f
Darshit Sarna <op.dare7@gmail.com>
2022-01-18 21:54:34 +0530

    session 4

---
+++ Session4 - logical operators and decision making/script.js
@@ -0,0 +1,131 @@
+// var a = 3; //0011
+// var b = 2; //0010
+
+// var c = a & b; //0010
+// console.log(c);
+
+// var d = a | b; //
+// console.log(d); //3
+
+// var e = a ^ b; //0001
+// console.log(e) // 1
+// a = 5; //0000 0101
+// var f = ~a; //1111 1010 how does JS convert this binary rep to integer ? Using 2's compliment.
+// console.log(f);//
+
+// a = 3; // 0000 0011
+// f = ~a;// 1111 1100
+// console.log(f); // -4
+
+// a = -4;
+// var f = ~a;
+// console.log(f);
+
+// var a = 5; // type - Number
+// var b = '5';// type - String
+// console.log(a === b) // type -Boolean value;//
+
+// var a = 10 > 5 && 2 == '2';
+// console.log(a); //
+
+// var a = !(3 === '3') || 5 < 2;
+// console.log(a); //true
+
+// var a = 5 < '4';
+// console.log(a);
+
+// // var a = 1 - '1';
+// var a = '2' / 2;
+// console.log(a); //10, 1, 0
+
+
+
+var condition1 = false;
+var condition2 = true;
+
+// if (condition1) {
+//     // statements
+//     console.log('Inside if')
+// }
+// else {
+//     console.log('Inside else');
+// }
+
+// if (condition1) {
+//     // statements.
+//     console.log('Inside if')
+// }
+// else if (condition2) {
+//     // even more statements.
+//     console.log('Inside else if');
+// }
+// else {
+//     console.log('Final else');
+// }
+
+// var a = 50
+// var b = 30
+// var c = 40
+// var exp1 = true;
+// if ((a > b && a > c) && (exp1)) {
+//     console.log('A is the largest');
+// }
+// else if (b > a && b > c) {
+//     console.log('B is the largest')
+// }
+// else if() {
+
+// }
+// else if () {
+
+// }
+// else {
+//     console.log('C is the largest');
+// }
+
+// var gender = ''
+// if (gender == 'male') {
+//     console.log('Gender is male')
+// }
+// else if (gender == 'female') {
+//     console.log('Gender is female')
+// }
+// else if (gender == 'trans') {
+//     console.log('Gender is trans')
+// }
+// else { //equivalent to default case
+//     console.log('User did not input gender FROM ELSE')
+// }
+
+
+// switch (gender) {
+//     case 'male':
+//         //multiple statements
+//         console.log('Gender is male');
+//         break;
+//     case 'female':
+//         //multiple statements
+//         console.log('Gender is female');
+//         break;
+//     case 'trans':
+//         console.log('Gender is trans');
+//         break;
+//     default:
+//         console.log('User did not input gender FROM DEFAULT');
+
+// }
+
+// var day = 2;
+
+// switch (day) {
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     default:
+//         console.log('no input')
+// }
+
+
